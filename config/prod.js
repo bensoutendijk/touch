ee78134b6efdea74a66a26c0b7879298fafac357
devaent <devaent1316@gmail.com>
2018-10-11 22:31:21 -0500

    added config keys

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  githubClientId: process.env.GITHUB_ID,
+  githubClientSecret: process.env.GITHUB_SECRET
+};
